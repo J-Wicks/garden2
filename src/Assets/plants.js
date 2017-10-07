30787a90195f14de07dbc1e03738b715d99faf6f
Jeremy Wicks <artfullu@gmail.com>
2017-10-07 15:13:06 -0400

    Added dummy info

--- src/Assets/plants.js
+++ src/Assets/plants.js
@@ -9,11 +9,11 @@ export default [
     name:"Baby's Breath",
     srcURL:'babysbreath',
     imgSrc: babysbreath,
-    distribution:"All over the place",
-    description:"Keep it watered",
-    taxonomy:"bad and bougie",
-    cooperNotes: 'i am cooper i am very smart',
-    scientificName:"baby flower",
+    distribution:`It is commercially cultivated in Peru, forming a major portion of that country's flower exports. \n Gypsophila paniculata is now widely distributed in North America. It is classed as an invasive species in places around the Great Lakes, such as the Sleeping Bear Dunes National Lakeshore and the Chicago region, and in the Pacific Northwest.`,
+    description:`It is a popular ornamental garden subject, and thrives in well-drained alkaline to neutral soils in full sun. Numerous cultivars have been selected, of which 'Bristol Fairy' and 'Rosenschleier' (with pale pink double flowers) have both gained the Royal Horticultural Society's Award of Garden Merit. \n G. paniculata is much used in the floristry trade (where it is often simply called "gyp") providing an effective backdrop for larger or more structured blooms.`,
+    taxonomy:"Coming Soon!",
+    cooperNotes: 'I am Cooper. I am smart and beautiful.',
+    scientificName:"Gypsophila paniculatar",
   },
   {
     name:"Orchid",
@@ -22,7 +22,7 @@ export default [
     distribution:"All over the place",
     description:"Keep it watered",
     taxonomy:"bad and bougie",
-    cooperNotes: 'i am cooper i am very smart',
+    cooperNotes: 'I am Cooper. I am smart and beautiful.',
     scientificName:"cool plant",
   },
   {
@@ -32,7 +32,7 @@ export default [
     distribution:"All over the place",
     description:"Keep it watered",
     taxonomy:"bad and bougie",
-    cooperNotes: 'i am cooper i am very smart',
+    cooperNotes: 'I am Cooper. I am smart and beautiful.',
     scientificName:"scary plant",
   },
   {
@@ -42,7 +42,7 @@ export default [
     distribution:"All over the place",
     description:"Keep it watered",
     taxonomy:"bad and bougie",
-    cooperNotes: 'i am cooper i am very smart',
+    cooperNotes: 'I am Cooper. I am smart and beautiful.',
     scientificName:"arachnid plant",
   },
   {
@@ -52,7 +52,7 @@ export default [
     distribution:"All over the place",
     description:"Keep it watered",
     taxonomy:"bad and bougie",
-    cooperNotes: 'i am cooper i am very smart',
+    cooperNotes: 'I am Cooper. I am smart and beautiful.',
     scientificName:"tasty plant",
   },
 ];
